3992515d659d9311be5af99698fe647e1a496c42
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-06-14 19:17:56 -1000

    5

--- js/app.js
+++ js/app.js
@@ -22,6 +22,8 @@ price1.innerHTML = '$8.99';
 
 //5. Target the button element with the id of 'discount1' and update the content to Free fries with purchase!
 
+discount1.innerHTML = 'Free fries with purchase!';
+
 //6. Create a div element with the id of 'item2' and update the content to Hendrick's Gin. Append this element to William Grand & Sons div.
 
 //7. Create a div element with the id of 'price2' and update the content to $34.99. Append this element to the Hendrick's Gin div.
